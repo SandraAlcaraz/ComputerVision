8f3eb446c2c4e954472bcd450783bcc42da5461e
GeraJuarez <geracjuarez@gmail.com>
2019-11-16 22:45:14 -0600

    add feature tag input as argument

--- emoji_feature_detection.py
+++ emoji_feature_detection.py
@@ -1,4 +1,3 @@
-import os
 import csv
 import pandas as pd
 from cv2 import cv2
@@ -10,25 +9,32 @@ def obtain_hog_features(img):
 
 def resize_image(img, d=130):
     dim = (d, d)
-    return cv2.resize(img, dim, interpolation = cv2.INTER_AREA) 
+    return cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
 if __name__ == '__main__':
+    import argparse
+    import glob
+
+    parser = argparse.ArgumentParser(description='Feature Extraction')
+    parser.add_argument('-p', type=str, action='store', dest='path', required=True, help='The path to read all images')
+    parser.add_argument('--tag', type=str, action='store', dest='tag', required=True, help='Tag name')
+    args = parser.parse_args()
     
     df = None
 
     try:
-        for file in os.listdir('roi/'):
-            if file.endswith('.jpg'):
-                img = cv2.imread(os.path.join('roi', file), cv2.IMREAD_COLOR)
-                # Skip non-square images
-                if img.shape[0] != img.shape[1]:
-                    continue
-                resized = resize_image(img)
-                features = obtain_hog_features(resized)
-                if df is None:
-                    df = pd.DataFrame(columns=['X{0}'.format(i) for i in list(range(features.shape[0]))])
-                df.loc[file] = features
-        df.to_csv('features.csv', header=True)
+        for file in glob.glob(f'{args.path}*.jpg'):
+            img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+            if img.shape[0] != img.shape[1]: continue # Skip non-square images
+            resized = resize_image(img)
+            features = obtain_hog_features(resized)
+
+            if df is None:
+                df = pd.DataFrame(columns=[f'X{i}' for i in list(range(features.shape[0]))])
+
+            df.loc[file] = features
+
+        df.to_csv(f'{args.tag}_features.csv', header=True)
 
     except Exception as e:
         print(e)
