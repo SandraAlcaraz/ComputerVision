aced2fc7ef679750416d032df260cc3e3a51eaee
GeraJuarez <geracjuarez@gmail.com>
2019-11-24 14:45:58 -0600

    remove emoji backgroun when overlayin to frames

--- emoji_detector.py
+++ emoji_detector.py
@@ -2,6 +2,7 @@ from emoji_segmentation import emoji_segmentation, get_circle_regions
 from skin_detector.cv_helpers import start_cv_video, plt_show_img
 
 from cv2 import cv2
+import numpy as np
 
 EMOJI_1 = None
 
@@ -14,7 +15,7 @@ def classify_emoji(img):
 
 def draw_emoji(frame, emoji_index, emoji_pos):
     print('Drawing...')
-    real_emoji = EMOJI_DICT.get(emoji_index)
+    real_emoji, inverse_mask = EMOJI_DICT.get(emoji_index)
     x, y, r = emoji_pos
     top = y - r
     bottom = y + r
@@ -22,10 +23,11 @@ def draw_emoji(frame, emoji_index, emoji_pos):
     rigth = x + r
 
     emoji = cv2.resize(real_emoji, (rigth - left, bottom - top))
+    inverse_mask = cv2.resize(inverse_mask, (rigth - left, bottom - top))
     overlap_area = frame[top:bottom, left:rigth]
-    merged_images = cv2.addWeighted(overlap_area, 0.4, emoji, 0.1, 0) 
-
-    frame[top:bottom, left:rigth] = merged_images
+    overlap_area = cv2.bitwise_and(overlap_area, overlap_area, mask=inverse_mask)
+    overlap_area = cv2.add(overlap_area, emoji)
+    frame[top:bottom, left:rigth] = overlap_area
 
     return frame
 
@@ -55,13 +57,17 @@ if __name__ == '__main__':
 
     try:
         for i in range(1, 7):
-            img = cv2.imread(f'emojis/{i}.png', cv2.IMREAD_COLOR)
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-            EMOJI_DICT[i] = img
+            original = cv2.imread(f'emojis/{i}.png', cv2.IMREAD_UNCHANGED)
+            r, g, b, a = cv2.split(original)
+            emoji = cv2.merge((r,g,b)).astype('uint8')
+            mask = a.astype('uint8')
+            inverse_mask = cv2.bitwise_not(mask)
+            emoji = cv2.bitwise_and(emoji, emoji, mask=mask)
+            EMOJI_DICT[i] = emoji, inverse_mask
 
         img = cv2.imread(args.src_img, cv2.IMREAD_COLOR)
-        plt_show_img(detect_emoji(img))
-        #start_cv_video(0, img_filter=detect_emoji)
+        #plt_show_img(detect_emoji(img))
+        start_cv_video(0, img_filter=detect_emoji)
 
     except Exception as error:
         print(error)
--- emoji_segmentation.py
+++ emoji_segmentation.py
@@ -42,6 +42,7 @@ def get_circle_regions(img, circles, shift=1):
         List[(image, (int,int,int))] -- A list of cropped images with its coordinates (x,y,r)
     """
     crops = []
+    if not circles: return []
     for circle in circles:
         x, y, r = circle
         r = int(r * shift)
